007a73d5827322e6c69b6a955dfc0bc513215bd1
Daniel Preisler <32717269+Krisp-Rat@users.noreply.github.com>
2023-09-27 17:51:33 -0400

    First draft. Needs minor functionality changes

---
+++ sign.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sign Up</title>
+    <link rel="stylesheet" type="text/css" href="sign_log.css"/>
+</head>
+<body>
+<div class="e64_10">
+    <div  class="e60_6">Sign Up</div>
+    <div class="e74_120">
+        <button type="button" value="create page" onclick="log_page()" class="e64_13">already have an account?<br>click here to log in</button>
+    </div>
+    <script type="text/JavaScript">
+
+        function log_page() {
+            window.location.href = "log.php";
+        }
+    </script>
+    <div class=e74_99>
+        <p>Username:</p>
+        <input type="text" class="e64_16"></input>
+    </div>
+
+    <div class=e74_100>
+        <p>Password:</p>
+        <input type="text" class="e64_16"></input>
+    </div>
+
+    <div class=e74_116>
+        <button type="button" class="e64_15">create account</button>
+    </div>
+</div>
+
+</body>
+</html>
