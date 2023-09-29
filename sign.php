2db2364a40984246714897ed59c77938d3211585
Daniel Preisler <32717269+Krisp-Rat@users.noreply.github.com>
2023-09-29 15:39:39 -0400

    Update sign.php

--- sign.php
+++ sign.php
@@ -6,12 +6,13 @@
     <link rel="stylesheet" type="text/css" href="sign_log.css"/>
 </head>
 <body>
+<div class="box"></div>
 <div class="e64_10">
     <div  class="e60_6">Sign Up</div>
     <div class="e74_120">
         <button type="button" value="create page" onclick="log_page()" class="e64_13">already have an account?<br>click here to log in</button>
     </div>
-    <script type="text/JavaScript">
+    <script type="text/JavaScript"> 
 
         function log_page() {
             window.location.href = "log.php";
@@ -19,12 +20,16 @@
     </script>
     <div class=e74_99>
         <p>Username:</p>
-        <input type="text" class="e64_16"></input>
+        <label>
+            <input type="text" class="e64_16">
+        </label>
     </div>
 
     <div class=e74_100>
         <p>Password:</p>
-        <input type="text" class="e64_16"></input>
+        <label>
+            <input type="text" class="e64_16">
+        </label>
     </div>
 
     <div class=e74_116>
