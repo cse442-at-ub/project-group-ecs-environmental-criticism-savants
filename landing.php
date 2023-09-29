842df5ac273f59eec8929cb355c254c11cab8384
Daniel12345678901 <dpreisler10@gmail.com>
2023-09-29 14:54:43 -0400

    Updated to fit current style

--- landing.php
+++ landing.php
@@ -7,13 +7,16 @@
 </head>
 <body>
 <div class=e64_42>
+    <div class="box">
+    </div>
     <span  class="e64_44">Welcome to Savant</span>
     <div class=e68_85>
         <div class="e64_45"></div><span  class="e64_48">Savant is an excellent tool to help:</span>
-        <ui  class="e64_50">
+        <ol  class="e64_50">
             <li> Organize events </li>
             <li> Keep track of school work </li>
-            <li>Keep track of birthday, anniversaries and other special events </li> </ui>
+            <li>Keep track of birthday, anniversaries and other special events </li>
+        </ol>
     </div>
     <div class=e68_86>
         <button type="button" value="create page" onclick="sign_page()" class="e64_77"> Click here to create an account </button>
@@ -26,7 +29,7 @@
     </script>
     <div class=e68_87>
 
-        <button type="button" value="create page" onclick="log_page()" class="e64_21">Click here to log in to an account </button>
+        <button type="button" value="create page" onclick="log_page()" class="e64_77">Click here to log in to an account </button>
     </div>
     <script type="text/JavaScript">
 
