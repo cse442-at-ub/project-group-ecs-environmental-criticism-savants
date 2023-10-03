017f6f215c109897d6f62ccd35a863e6852d7d0c
Daniel12345678901 <dpreisler10@gmail.com>
2023-10-03 01:33:09 -0400

    Messing around with HTML/JS features

--- landing.php
+++ landing.php
@@ -3,10 +3,50 @@
 <head>
     <meta charset="UTF-8">
     <title>Savant</title>
-    <link rel="stylesheet" type="text/css" href="landing.css"/>
+    <link rel="stylesheet" type="text/css" href="landing.css" id = "style"/>
 </head>
+
 <body>
     <div class="banner"></div>
+
+    <div>
+        <button class ="switch" type="button" onclick= "swap_dark()" id ="dark">Dark Mode</button>
+        <button hidden="hidden" class ="switch" type="button" onclick= "swap_light()" id ='light'>Light Mode</button>
+    </div>
+    <script type="text/JavaScript">
+        function swap_dark() {
+            let style = document.getElementById('style');
+            let light = document.getElementById('light');
+            let dark = document.getElementById('dark');
+            style.setAttribute('href', 'landing_dark.css');
+            let a = document.createAttribute("hidden") ;
+            a.value = "hidden"
+            dark.setAttributeNode(a)
+            light.removeAttribute("hidden")
+            localStorage.setItem('mode', 'dark');
+
+        }
+        function swap_light() {
+            let style = document.getElementById('style');
+            let light = document.getElementById('light');
+            let dark = document.getElementById('dark');
+            style.setAttribute('href', 'landing.css');
+            let a = document.createAttribute("hidden") ;
+            a.value = "hidden"
+            light.setAttributeNode(a)
+            dark.removeAttribute("hidden")
+            localStorage.setItem('mode', 'light');
+        }
+
+        function dark_check(){
+            let d = localStorage.getItem('mode');
+            console.log(d)
+            if (d === 'dark') {
+                swap_dark()
+            }
+        }
+        dark_check()
+    </script>
 
     <div class="wel">Welcome to Savant</div>
 
@@ -23,16 +63,15 @@
         <button type="button" value="create page" onclick="sign_page()" class="create"> Click here to create an account </button>
     </div>
 
-    <script type="text/JavaScript">
-        function sign_page() {window.location.href = "sign.php";}
-    </script>
-
     <div>
         <button type="button" value="create page" onclick="log_page()" class="log">Click here to log in to an account </button>
     </div>
 
     <script type="text/JavaScript">
-        function log_page() {window.location.href = "log.php";}
+        function log_page() {
+            window.location.href = "log.php";
+        }
+        function sign_page() {window.location.href = "sign.php";}
     </script>
 </body>
 </html>
--- log.php
+++ log.php
@@ -3,10 +3,48 @@
 <head>
     <meta charset="UTF-8">
     <title>Log In</title>
-    <link rel="stylesheet" type="text/css" href="sign_log.css"/>
+    <link rel="stylesheet" type="text/css" href="sign_log.css" id="log"/>
 </head>
 <body>
 <div class="banner"> </div>
+<div>
+    <button class ="switch" type="button" onclick= "swap_dark()" id ="dark">Dark Mode</button>
+    <button hidden="hidden" class ="switch" type="button" onclick= "swap_light()" id ='light'>Light Mode</button>
+</div>
+<script type="text/JavaScript">
+    function swap_dark() {
+        let style = document.getElementById('log');
+        let light = document.getElementById('light');
+        let dark = document.getElementById('dark');
+        style.setAttribute('href', 'sign_log_dark.css');
+        let a = document.createAttribute("hidden") ;
+        a.value = "hidden"
+        dark.setAttributeNode(a)
+        light.removeAttribute("hidden")
+        localStorage.setItem('mode', 'dark');
+    }
+    function swap_light() {
+        let style = document.getElementById('log');
+        let light = document.getElementById('light');
+        let dark = document.getElementById('dark');
+        style.setAttribute('href', 'sign_log.css');
+        let a = document.createAttribute("hidden") ;
+        a.value = "hidden"
+        light.setAttributeNode(a)
+        dark.removeAttribute("hidden")
+        localStorage.setItem('mode', 'light');
+    }
+
+    function dark_check(){
+        let d = localStorage.getItem('mode');
+        console.log(d)
+        if (d === 'dark') {
+            swap_dark()
+        }
+    }
+
+    dark_check()
+</script>
 <div class="main">
     <div  class="title">Log In</div>
 
