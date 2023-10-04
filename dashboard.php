9b7fabd879ae2cb8b91bf7e1dae7dce71f626953
Daniel Preisler <32717269+Krisp-Rat@users.noreply.github.com>
2023-10-04 17:48:23 -0400

    Basic body elements

---
+++ dashboard.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Savant</title>
+    <link rel="stylesheet" type="text/css" href="dash.css"/>
+</head>
+<body>
+    <div class="banner">
+        <p class="wel">Welcome back, </p>
+    </div>
+    <div class="main">
+        <div class="main_banner"></div>
+    </div>
+    <div class="year"></div>
+</body>
+</html>
