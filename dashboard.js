8e09bebc32d854314570db524516279013463638
Daniel Preisler <32717269+Krisp-Rat@users.noreply.github.com>
2023-10-05 19:44:24 -0400

    Added date changing functionality

---
+++ dashboard.js
@@ -0,0 +1,44 @@
+// Sets the day and year elements, highlights the correct month when dashboard.php loads
+function set(time) {
+    let date = time.toDateString().split(' ');
+    let day = document.getElementById("day");
+    let year = document.getElementById("year");
+    year.innerHTML = date[3];
+    day.innerHTML = date[0] + ", " + date[1] + " " + date[2];
+    document.getElementById(date[1]).style.background = "#80B4F0"
+}
+
+// Increments or decreases the year elements on click
+function yearUpdate(cur){
+    const el = document.getElementById("year");
+    let up = parseInt(el.innerHTML) + cur;
+    var day = document.getElementById("day").innerHTML.slice(5);
+    day = day + " " + up;
+    set(new Date(day));
+}
+
+// Changes the current month based off of which month was selected
+function monthUpdate (cur) {
+    let day = document.getElementById("day").innerHTML.slice(5).split(" ");
+    let year = document.getElementById("year").innerHTML;
+    document.getElementById(day[0]).style.background = "#E5E5E5";
+    let date = cur + " " + day[1] + " " + year;
+    set(new Date(date));
+}
+
+// Inrements or decreases date
+function dayUpdate(cur) {
+    var day = document.getElementById("day").innerHTML.slice(5);
+    day = day + " " + document.getElementById("year").innerHTML;
+    let date = new Date(day);
+    let month1 = date.toDateString().split(' ')[1];
+    date.setDate(date.getDate()+ cur)
+    let month2 = date.toDateString().split(' ')[1];
+    if (month1 != month2) {
+        document.getElementById(month1).style.background = "#E5E5E5";
+    }
+    set(date);
+
+}
+
+
--- dashboard.php
+++ dashboard.php
@@ -3,9 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Savant</title>
+    <script src="dashboard.js"></script>
     <link rel="stylesheet" type="text/css" href="dash.css"/>
 </head>
-<body>
+<body onload="set(new Date())">
     <div class="banner">
         <p class="wel">Welcome back, </p>
     </div>
@@ -15,44 +16,35 @@
         <div class="main_banner">
             <p class="view">Card View</p>
         </div>
-        <div class="day" id="day">Thursday, Nov 31</div>
-        <button type="button" class="button1"><</button>
-        <button type="button" class="button2">></button>
+        <div class="day" id="day"></div>
+        <button type="button" class="button1" onclick="dayUpdate(-1)"><</button>
+        <button type="button" class="button2" onclick="dayUpdate(1)">></button>
         <div id="cards"></div>
     </div>
 
+
+
     <div class="year">
         <div>
-            <div class="year_date" id="year">2023</div>
-            <button type="button" class="button3" onclick="year(-1)"><</button>
-            <button type="button" class="button4" onclick="year(1)">></button>
+            <div class="year_date" id="year"></div>
+            <button type="button" class="button3" onclick="yearUpdate(-1)"><</button>
+            <button type="button" class="button4" onclick="yearUpdate(1)">></button>
         </div>
-
-        <script type="text/javascript">
-            function year(cur){
-                const el = document.getElementById("year");
-                el.innerHTML = parseInt(el.innerHTML) + cur
-            }
-        </script>
 
         <div>
-            <button type="button" class="row1" id="jan" onclick="monthchange('jan')">Jan</button>
-            <button type="button" class="row1" id="feb" onclick="monthchange('feb')">Feb</button>
-            <button type="button" class="row2" id="mar" onclick="monthchange('mar')">Mar</button>
-            <button type="button" class="row2" id="apr" onclick="monthchange('apr')">Apr</button>
-            <button type="button" class="row3" id="may" onclick="monthchange('may')">May</button>
-            <button type="button" class="row3" id="jun" onclick="monthchange('jun')">Jun</button>
-            <button type="button" class="row4" id="jul" onclick="monthchange('jul')">Jul</button>
-            <button type="button" class="row4" id="aug" onclick="monthchange('aug')">Aug</button>
-            <button type="button" class="row5" id="sep" onclick="monthchange('sep')">Sep</button>
-            <button type="button" class="row5" id="oct" onclick="monthchange('oct')">Oct</button>
-            <button type="button" class="row6" id="nov" onclick="monthchange('nov')">Nov</button>
-            <button type="button" class="row6" id="dec" onclick="monthchange('dec')">Dec</button>
+            <button type="button" class="row1" id="Jan" onclick="monthUpdate('Jan')">Jan</button>
+            <button type="button" class="row1" id="Feb" onclick="monthUpdate('Feb')">Feb</button>
+            <button type="button" class="row2" id="Mar" onclick="monthUpdate('Mar')">Mar</button>
+            <button type="button" class="row2" id="Apr" onclick="monthUpdate('Apr')">Apr</button>
+            <button type="button" class="row3" id="May" onclick="monthUpdate('May')">May</button>
+            <button type="button" class="row3" id="Jun" onclick="monthUpdate('Jun')">Jun</button>
+            <button type="button" class="row4" id="Jul" onclick="monthUpdate('Jul')">Jul</button>
+            <button type="button" class="row4" id="Aug" onclick="monthUpdate('Aug')">Aug</button>
+            <button type="button" class="row5" id="Sep" onclick="monthUpdate('Sep')">Sep</button>
+            <button type="button" class="row5" id="Oct" onclick="monthUpdate('Oct')">Oct</button>
+            <button type="button" class="row6" id="Nov" onclick="monthUpdate('Nov')">Nov</button>
+            <button type="button" class="row6" id="Dec" onclick="monthUpdate('Dec')">Dec</button>
         </div>
-        <script type="text/javascript">
-            function monthchange(cur){
-            }
-        </script>
 
     </div>
 </body>
