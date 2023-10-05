f4de67715429f407f8724202f76b4c46260fe6cc
Daniel12345678901 <dpreisler10@gmail.com>
2023-10-05 16:52:29 -0400

    Added year button functionality

--- dashboard.php
+++ dashboard.php
@@ -23,25 +23,36 @@
 
     <div class="year">
         <div>
-            <div class="year_date" id="day">2023</div>
-            <button type="button" class="button3"><</button>
-            <button type="button" class="button4">></button>
+            <div class="year_date" id="year">2023</div>
+            <button type="button" class="button3" onclick="year(-1)"><</button>
+            <button type="button" class="button4" onclick="year(1)">></button>
         </div>
 
+        <script type="text/javascript">
+            function year(cur){
+                const el = document.getElementById("year");
+                el.innerHTML = parseInt(el.innerHTML) + cur
+            }
+        </script>
+
         <div>
-            <button type="button" class="row1">Jan</button>
-            <button type="button" class="row1">Feb</button>
-            <button type="button" class="row2">Mar</button>
-            <button type="button" class="row2">Apr</button>
-            <button type="button" class="row3">May</button>
-            <button type="button" class="row3">Jun</button>
-            <button type="button" class="row4">Jul</button>
-            <button type="button" class="row4">Aug</button>
-            <button type="button" class="row5">Sep</button>
-            <button type="button" class="row5">Oct</button>
-            <button type="button" class="row6">Noc</button>
-            <button type="button" class="row6">Dec</button>
+            <button type="button" class="row1" id="jan" onclick="monthchange('jan')">Jan</button>
+            <button type="button" class="row1" id="feb" onclick="monthchange('feb')">Feb</button>
+            <button type="button" class="row2" id="mar" onclick="monthchange('mar')">Mar</button>
+            <button type="button" class="row2" id="apr" onclick="monthchange('apr')">Apr</button>
+            <button type="button" class="row3" id="may" onclick="monthchange('may')">May</button>
+            <button type="button" class="row3" id="jun" onclick="monthchange('jun')">Jun</button>
+            <button type="button" class="row4" id="jul" onclick="monthchange('jul')">Jul</button>
+            <button type="button" class="row4" id="aug" onclick="monthchange('aug')">Aug</button>
+            <button type="button" class="row5" id="sep" onclick="monthchange('sep')">Sep</button>
+            <button type="button" class="row5" id="oct" onclick="monthchange('oct')">Oct</button>
+            <button type="button" class="row6" id="nov" onclick="monthchange('nov')">Nov</button>
+            <button type="button" class="row6" id="dec" onclick="monthchange('dec')">Dec</button>
         </div>
+        <script type="text/javascript">
+            function monthchange(cur){
+            }
+        </script>
 
     </div>
 </body>
