f21fa51e26c50f87d5fea7c0a4ae63299fee2fc2
Daniel Preisler <32717269+Krisp-Rat@users.noreply.github.com>
2023-10-04 21:01:33 -0400

    Basic body elements: year

--- dashboard.php
+++ dashboard.php
@@ -9,9 +9,29 @@
     <div class="banner">
         <p class="wel">Welcome back, </p>
     </div>
+
     <div class="main">
-        <div class="main_banner"></div>
+        <div class="main_banner">
+            <p class="view">Card View</p>
+        </div>
+        <div class="day" id="day">Thursday, Nov 31</div>
+        <button type="button" class="button1"><</button>
+        <button type="button" class="button2">></button>
+        <div id="cards"></div>
     </div>
-    <div class="year"></div>
+
+    <div class="year">
+        <div>
+            <div class="year_date" id="day">2023</div>
+            <button type="button" class="button3"><</button>
+            <button type="button" class="button4">></button>
+        </div>
+
+        <div>
+
+
+        </div>
+
+    </div>
 </body>
 </html>
