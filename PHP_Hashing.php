d8ec5a511abf223a411c2d035828e861aeb1c14d
Mike <mdtornab@buffalo.edu>
2023-10-15 22:24:15 -0400

    Rudimentary code for verifying existence of users, password verification for existing users, checking password criteria, and data store added. Requires connection to database before code can be tested.

--- PHP_Hashing.php
+++ PHP_Hashing.php
@@ -1,8 +1,4 @@
 <?php
-//declaration of user input variables for log in/sign up
-$username;
-$password;
-
 //When creating an account, this will check for incorrect inputs
 function CheckUsernameAndPassword ($User, $Pass, $rePass) {
     if (VerifyExistingUsername($User)){return "taken";}
@@ -16,26 +12,54 @@ function CheckUsernameAndPassword ($User, $Pass, $rePass) {
 //This function will check the username on sign in to make sure it exists within the database
 function VerifyExistingUsername($User): bool {
     //connect to the database
+
     //check where usernames are stored in the database
+    $sql = "SELECT username FROM users WHERE username = '" + $User + "'" ;
+    $request = mysql_query($sql);
     //confirm that a username in the DB matches the entered one
-    //if there is a match, return TRUE
-    //else FALSE
-    //Close database connection
-    return False;
+    if (!($request == null)) {
+        //close database connection
+
+        //if there is a match, return TRUE
+        return True;
+    }
+    //else FALSE because no user of that name exists
+    else{
+        //close database connection
+
+        //
+        return False;
+    }
 }
 
 //Function which will verify when logging in if the password entered matches a user
 function VerifyExistingPassword ($User, $Pass): bool {
-    //use password_verify($password, $hashed_password) function
-
     //Connect to database
+
     //verify the username exists in DataBase
+    if (!VerifyExistingUsername($User)){
+        //get hashed password for user
+        $request = "SELECT hashedpass FROM users WHERE username = '" + $User +"'";
+        $hashed = mysql_query($request);
+        // check if the inputted password matches the hashed
+        if (password_verify($Pass, $hashed)) {
+            //close database connection
+
+            return True;
+        }
+        // else password input is wrong
+        else {
+            //Close database connection
+
+            return False;
+        }
+    }
     //if it doesn't, error, user doesn't exist
-    //else, pass the stored hashed password as $hashed_password) and $Pass as $password for password_verify
-    $UserExists = password_verify($Pass); // $(RETRIEVED PASSWORD HASH STORED IN DATABASE) //); PASSWORD_VERIFY IS MISSING A PARAMETER, REMEMBER TO ADJUST PROPER WHEN IMPLEMENTING FULLY
-    //if false, error, wrong password
-    //else correct user and password, log in the user.
-    //close database connection
+    else{
+        //close database connection
+
+        return False;
+    }
 }
 
 //Function which will take a username and password during sign in and store them
@@ -44,9 +68,14 @@ function StoreUserDataSignLog ($User, $Pass) {
     //use password_hash function to salt and hash the password
     $encryptedPassword = password_hash($Pass, PASSWORD_DEFAULT);
     //connect to database
+
     //store the username in the respective column
     //store the password in the respective column
+    $request = "INSERT INTO users (username, hashedpass) VALUES ('" + $User "', '" + $encryptedPassword +"')";
+    mysql_query($request);
+
     //close database connection
+
 }
 
 ?>
