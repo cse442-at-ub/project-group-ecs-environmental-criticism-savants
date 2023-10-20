4b2f7a85d64014d12f25709acf0bd839bf6e72a9
Chris <chrisjak@buffalo.edu>
2023-10-20 02:28:17 -0400

    Organized some files, and completed the front end functionality for the change password alerts depending on user input. Currently awaiting implementation of back-end flags to display correct alert.

--- change_password.php
+++ change_password.php
@@ -1,7 +1,13 @@
 <?php
     include "PHP_HASHING.php";
+    include "Credential-Template.php";
     include "DBConnection-Function.php";
+
+    $dom = new DOMDocument();
+    $dom->loadHTMLFile("change_password.html");
 
     $currPass = $_POST["current_password"];
     $newPass = $_POST["new_password"];
-    $reentered = $_POST["reentered_password"];
+    $reentered = $_POST["reenter_password"];
+
+    echo $dom->saveHTML();
