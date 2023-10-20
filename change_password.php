280a763c7f22cd161aab964adf09930fa842a6a2
Chris <chrisjak@buffalo.edu>
2023-10-20 13:21:48 -0400

    Completed the functionality for changing a user's password in the database on a fixed username: "iD_0m". Signing in with the new password should work
    Also included error popups for passwords not meeting criteria.
    
    Currently awaiting functionality for tokens, so this works on all possible users.

--- change_password.php
+++ change_password.php
@@ -1,13 +1,26 @@
 <?php
-    include "PHP_HASHING.php";
     include "Credential-Template.php";
     include "DBConnection-Function.php";
+    include "Settings-Functions.php";
 
     $dom = new DOMDocument();
     $dom->loadHTMLFile("change_password.html");
 
+    $user = "iD_0m";
     $currPass = $_POST["current_password"];
     $newPass = $_POST["new_password"];
     $reentered = $_POST["reenter_password"];
+
+    $conn = get_database_connection(HOST, H_USERNAME, H_PASSWORD, DATABASE);
+    $result = CreateChangePasswordFlag($user, $currPass, $newPass, $reentered, $conn);
+    $conn = null;
+    // if it works properly unhide "successfully changed pass popup"
+    if($result == "done"){
+        $conn = get_database_connection(HOST, H_USERNAME, H_PASSWORD, DATABASE);
+        StoreNewPassword($user, $newPass, $conn);
+        $conn = null;
+    }
+    $node1 = $dom->getElementById($result);
+    $node1->removeAttribute("hidden");
 
     echo $dom->saveHTML();
