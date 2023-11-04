7d49588ac5c70233906909c059794f80d2323c29
Mike <mdtornab@buffalo.edu>
2023-11-04 16:33:56 -0400

    logic for adding user task to that user's task database

--- UserTaskStorage.php
+++ UserTaskStorage.php
@@ -1,5 +1,4 @@
 <?php
-use MongoDB\Driver\Query;
 function CreateTaskDatabase($User, $conn) {
     $Query = $conn->prepare("CREATE TABLE ? (name TEXT, deadline TEXT, description TEXT, recurrence TEXT, priority TEXT)");
     $Query->execute([$User]);
@@ -9,7 +8,8 @@ function retrieveDeadline($User, $task, $conn){
 
 }
 
-function addtask($User, $Taskname, $TaskDescription, $TaskDeadline, $conn) {
-
+function addtask($User, $Taskname, $TaskDescription, $TaskDeadline, $Recurrence, $Priority, $conn) {
+    $query = $conn->prepare("INSERT INTO ? (name, deadline, description, recurrence, priority) VALUES (?, ?, ?, ?, ?)");
+    $query->execute([$Taskname, $TaskDescription, $TaskDeadline, $Recurrence, $Priority])
 }
 ?>
