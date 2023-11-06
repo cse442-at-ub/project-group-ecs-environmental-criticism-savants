4d8298dbb25c0d5d7bcf9785863f3e6b6774d79b
mdtornab <112581056+mdtornab@users.noreply.github.com>
2023-11-06 16:29:25 -0500

    Update Credential-Template.php

--- Credential-Template.php
+++ Credential-Template.php
@@ -1,6 +1,6 @@
 <?php
-define("HOST", "oceanus.cse.buffalo.edu");
-define("H_USERNAME", "rar38");
-define("H_PASSWORD", "50357560");
-define("DATABASE", "cse442_2023_fall_team_v_db");
+define("HOST", "PLACEHOLDER");
+define("H_USERNAME", "PLACEHOLDER");
+define("H_PASSWORD", "PLACEHOLDER");
+define("DATABASE", "PLACEHOLDER");
 ?>
