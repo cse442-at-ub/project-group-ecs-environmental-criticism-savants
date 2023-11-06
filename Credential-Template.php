a1eb62cbdfd9363fd326c7a6ef1ad69b6f0d0f44
Nitin Pai <nitinpai300@gmail.com>
2023-11-06 18:57:34 -0500

    changing profile

--- Credential-Template.php
+++ Credential-Template.php
@@ -1,6 +1,14 @@
+
 <?php
+define("HOST", "localhost");
+define("H_USERNAME", "root");
+define("H_PASSWORD", "");
+define("DATABASE", "users");
+?>
+<!-- <?php
 define("HOST", "oceanus.cse.buffalo.edu");
 define("H_USERNAME", "rar38");
 define("H_PASSWORD", "50357560");
 define("DATABASE", "cse442_2023_fall_team_v_db");
-?>
+?> 
+CHANGEME!-->
