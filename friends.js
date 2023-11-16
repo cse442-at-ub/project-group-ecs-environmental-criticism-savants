87f07b8228405922cd193e105b858e559d91a65f
Daniel Preisler <32717269+Krisp-Rat@users.noreply.github.com>
2023-11-16 17:41:58 -0500

    Formatted UI for mobile

--- friends.js
+++ friends.js
@@ -2,11 +2,3 @@ function nav(filename){
     window.location.href = filename;
 }
 
-window.addEventListener('resize', function() {
-    var contentEl = document.getElementById('body');
-    if (window.innerWidth < 1920) {
-        contentEl.style.overflowY = 'auto';
-    } else {
-        contentEl.style.overflowY = 'hidden';
-    }
-});
