9034239d1c7d037033fe812f9f2a5084a38de20b
Mike <mdtornab@buffalo.edu>
2023-11-21 13:44:46 -0500

    Functionality for adding and removing a friend, function prototype for friend retrieval

--- FriendsToDatabase.php
+++ FriendsToDatabase.php
@@ -1,11 +1,17 @@
 <?php
 
 function StoreFriend($username, $friendname, $birthday, $conn) {
-    
+    $request = $conn->prepare("INSERT INTO friends (username, friendname, birthday) VALUES (?,?,?)");
+    $request->execute([$username,$friendname,$birthday]);
 }
 
 function RemoveFriend($username, $friendname, $birthday,  $conn) {
+    $request = $conn->prepare("DELETE FROM friends WHERE username = ? AND friendname = ? AND birthday = ?");
+    $request->execute([$username, $friendname, $birthday]);
+}
 
+RetrieveFriends($username, $conn){
+    
 }
 
 ?>
