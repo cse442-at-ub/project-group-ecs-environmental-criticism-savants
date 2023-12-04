aefe060ee4d535578ed2ecf84e072f5454c06aee
Nitin Pai <nitinpai300@gmail.com>
2023-12-04 08:29:03 -0500

    current working state

--- ModifyDarkModeState.php
+++ ModifyDarkModeState.php
@@ -1,22 +1,22 @@
 <?php
 #1 is dark, 0 is light
-function AddDefaultDarkMode($username, $conn) {
-    $query = $conn->prepare("INSERT INTO darkmode (user, state) VALUES (?,?)");
+function set_default_theme($username, $conn) {
+    $query = $conn->prepare("INSERT INTO theme (user, preference) VALUES (?,?)");
     $query->execute([$username, 0]);
 }
 
 function retrievedarkmodestate($username, $conn){
-    $query = $conn->prepare("SELECT user, state FROM darkmode WHERE user = ?");
+    $query = $conn->prepare("SELECT user, preference FROM theme WHERE user = ?");
     $query->execute([$username]);
     $endarray = [];
     foreach ($query->fetchAll(PDO::FETCH_ASSOC) as $task) {
         $endarray[] = $task;
     }
-    return $endarray;
+    return $endarray[0]['preference'];
 }
 
 function updatedarkmodestate($username, $state, $conn){
-    $query = $conn->prepare("UPDATE darkmode SET state = ? WHERE user = ?");
+    $query = $conn->prepare("UPDATE theme SET preference = ? WHERE user = ?");
     $query->execute([$state, $username]);
 }
 ?>
--- switchmode.php
+++ switchmode.php
@@ -1,6 +1,7 @@
 <?php
     include "time_change.php";
     include "ModifyDarkModeState.php";
+    
     if (!isset($_SESSION['user_token'])){
         session_start();
     }
@@ -15,24 +16,14 @@
         $username = $_SESSION['user_username']; // Retrieve the username from the session
     }
     $currstate = retrievedarkmodestate($username, $conn);
-    if($currstate==1){
-        $node0 = $dom->getElementbyId("stylesheets");
-        $node0->setAttribute('href',"dash-dark.css");
+    if($currstate==0){
+        updatedarkmodestate($username, 1, $conn);
     }
     else{
-        $node0 = $dom->getElementbyId("stylesheets");
-        $node0->setAttribute('href',"dash.css");
+        updatedarkmodestate($username, 0, $conn);
     }
-    $node = $dom->getElementbyId("user");
-    $node->textContent = "Hello " . $username;
-    $node2 = $dom->getElementbyId("left1");
-    $node3 = $dom->getElementbyId("left2");
-    $node2->textContent = "<";
-    $node3->textContent = "<";
-
-    $node6 = $dom->getElementById("data-grab");
-    $node6->textContent = $username;
-
-    echo $dom->saveHTML();
+    
+    header("Location: dashboard.php");
+    die();
 
 ?>
