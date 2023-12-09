df9b7a7c3aa8b6d81cdb52b5e2f6c08fb2ca1d82
Daniel Preisler <32717269+Krisp-Rat@users.noreply.github.com>
2023-12-09 00:26:50 -0500

    moved JS function for dark mode. Small graphical glitch.

--- dashboard.js
+++ dashboard.js
@@ -273,17 +273,3 @@ window.addEventListener('resize', function() {
 
   //temp dark mode setter
 
-  function changetheme(){
-    var stylechanger = document.getElementById("stylesheets");
-    if(stylechanger.href.includes("dash.css")){
-        stylechanger.href = 'dash-dark.css';
-        localStorage.setItem('theme', '1');
-    }
-    else{
-        stylechanger.href = "dash.css";
-        localStorage.setItem('theme', '0');
-    }
-    
-    var submitter = document.getElementById("submitt");
-    submitter.submit();
-  }
